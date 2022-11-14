a8e184f6b6c166781079f3cb0b90fcfd2043558a
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-11-14 12:53:11 -0300

    esquecu

--- source/meta/state/PlayState.hx
+++ source/meta/state/PlayState.hx
@@ -2451,11 +2451,11 @@ class PlayState extends MusicBeatState
 			powerupVisuals("idle");
 		}
 
-		songMusic = new FlxSound().loadEmbedded(Paths.inst(SONG.song)), false, true);
+		songMusic = new FlxSound().loadEmbedded(Paths.inst(SONG.song), false, true);
 		songMusic.volume = 0.75;
 
 		if (SONG.needsVoices)
-			vocals = new FlxSound().loadEmbedded(Paths.voices(SONG.song)), false, true);
+			vocals = new FlxSound().loadEmbedded(Paths.voices(SONG.song), false, true);
 		else
 			vocals = new FlxSound();
 		vocals.volume = 0.75;
